8e91d3c2a1102c7377d665b2637320bc32944004
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-05-04 12:38:37 +0530

    Installed css-loader and style-loader packages, updated webpack.config.base.js

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -15,7 +15,13 @@ module.exports = {
         exclude: /node-modules/,
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-transform-class-properties"],
         },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
       },
     ],
   },
